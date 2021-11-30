f7bc3ce5e577232d614123414cbb206cdedfb975
Prasad L Telkikar <telkikar.prasad@gmail.com>
2021-11-30 23:19:58 +0530

    initial commit

---
+++ LeetCode/Easy-Problems/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Easy_Problems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var target = int.Parse(Console.ReadLine());
+            Program pObject = new Program();
+            var result = pObject.TargetIndices(inputs, target);
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            var result = new List<int>();
+            Array.Sort(nums);
+            for(int i = 0; i < nums.Length;i++)
+            {
+                if (nums[i] == target)
+                    result.Add(i); 
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/TwoSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class TwoSum
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var target = int.Parse(Console.ReadLine());
+
+            TwoSum ts = new TwoSum();
+            var result = ts.TwoSumFun(nums, target);
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        public int[] TwoSumFun(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var secondNumber = target - nums[i];
+                var secondNumberIndex = Array.IndexOf(nums, secondNumber);
+                if (i == secondNumberIndex)
+                    continue;
+                if (secondNumberIndex != -1)
+                {
+                    result[0] = i;
+                    result[1] = secondNumberIndex;
+                    return result;
+                }
+            }
+            return result;
+        }
+    }
+}
