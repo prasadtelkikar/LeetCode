2c930ce794f13a8239be3579462f80c43177307c
Prasad L Telkikar <telkikar.prasad@gmail.com>
2021-12-02 22:01:00 +0530

    Solved missing number

---
+++ LeetCode/Easy-Problems/MissingNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class MissingNumber
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int result = MissingNumberFunc(nums); 
+            Console.WriteLine(result);
+        }
+
+        private static int MissingNumberFunc(int[] nums)
+        {
+            var result = 0;
+            if(nums.Contains(nums.Length))
+            {
+                var length = nums.Length + 1;
+                int[] store = new int[length];
+                Array.Sort(nums);
+                for (int i = 0; i < length; i++)
+                {
+                    if (nums[i] != i)
+                    {
+                        result = i;
+                        break;
+                    }
+                }
+            }
+            else
+                result = nums.Length;
+            return result; 
+        }
+    }
+}
