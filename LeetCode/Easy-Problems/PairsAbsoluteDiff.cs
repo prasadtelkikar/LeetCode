952ba167623173be177f3957d160ea6bf352ed81
Prasad L Telkikar <telkikar.prasad@gmail.com>
2021-12-26 00:04:23 +0530

    Solved pair with absolute difference

---
+++ LeetCode/Easy-Problems/PairsAbsoluteDiff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class PairsAbsoluteDiff
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var k = int.Parse(Console.ReadLine());
+            var result = CountKDifference(nums, k);
+            Console.WriteLine(result);
+        }
+
+        private static object CountKDifference(int[] nums, int k)
+        {
+            var count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n1 = nums[i] + k;
+                var n2 = nums[i] - k;
+                var newArray = nums.Skip(i);
+
+                var countn = newArray.Count(x => x == n1 || x == n2);
+
+                count += countn;
+            }
+            return count;
+        }
+    }
+}
