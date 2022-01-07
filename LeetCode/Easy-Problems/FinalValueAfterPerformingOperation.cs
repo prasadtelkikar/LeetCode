c14c60c39560832ff32fd8b4892a43e4febd0c34
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-07 22:56:14 +0530

    Easy: Solved two problems from Array

---
+++ LeetCode/Easy-Problems/FinalValueAfterPerformingOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class FinalValueAfterPerformingOperation
+    {
+        public static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            FinalValueAfterPerformingOperation operations = new FinalValueAfterPerformingOperation();
+            var result = operations.FinalValueAfterOperations(inputs);
+            Console.WriteLine(result);
+        }
+
+        private int FinalValueAfterOperations(string[] operations)
+        {
+            var result = 0;
+            foreach (var operation in operations)
+            {
+                if (operation.Contains("++"))
+                    result++;
+                else
+                    result--;
+            }
+            return result;
+        }
+
+        //TODO: Try other approaches.
+    }
+}
---
+++ LeetCode/Easy-Problems/MaxNoInSentences.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class MaxNoInSentences
+    {
+        public static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(", ").Select(x => x.Trim()).ToArray();
+            MaxNoInSentences maxLengthInSentence = new MaxNoInSentences();
+            var result = maxLengthInSentence.MostWordsFound(inputs);
+            Console.WriteLine(inputs.Length);
+        }
+        public int MostWordsFound(string[] sentences)
+        {
+            var maxLength = int.MinValue;
+            foreach (var sentence in sentences)
+            {
+                var wordLength = sentence.Split(' ').Length;
+                if(maxLength < wordLength)
+                    maxLength = wordLength;
+            }
+            return maxLength;
+        }
+
+        public int MostWordsFoundLinq(string[] sentences)
+        {
+            var maxLength = sentences
+                .Select((x, i) => new { Length = x.Split().Length })
+                .OrderByDescending(x => x.Length)
+                .First()
+                .Length;
+            return maxLength;
+        }
+
+        //MSDN Help
+        public int MostWordsAggregate(string[] sentences)
+        {
+            var result = sentences.Aggregate("", (longest, next) => next.Split().Length > longest.Split().Length ? next : longest);
+            return result.Split().Length;
+        }
+
+        //Cheating
+        public int MostWordsSelectLinq(string[] sentences)
+        {
+            var maxLength = sentences
+                .Select(x => x.Split().Length).
+                Max();
+            return maxLength;
+        }
+    }
+}
