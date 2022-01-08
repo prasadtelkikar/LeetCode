56a9c9c4376642e9cbfdae447c5187769c0ce221
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-08 22:55:31 +0530

    Two easiest array problem solved with multiple approaches.

---
+++ LeetCode/Easy-Problems/ArrayFromPermutation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class ArrayFromPermutation
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            var result = BuildArray(nums);
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        private static int[] BuildArray(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+                result[i] = nums[nums[i]];
+            return result;
+        }
+        
+        private static int[] BuildArraySelectLinq(int[] nums)
+        {
+            return nums.Select(x => nums[x]).ToArray();
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/SameArrayConcatenation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class SameArrayConcatenation
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            var result = GetConcatenation(input);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        //Efficient than others
+        private static int[] GetConcatenation(int[] input)
+        {
+            var length = input.Length;
+            var result = new int[length*2];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = input[i];
+                result[i+length] = input[i];
+            }
+            return result;
+            throw new NotImplementedException();
+        }
+
+        private static int[] GetConcatenationList(int[] nums)
+        {
+            var list = new List<int>();
+            list.AddRange(nums);
+            list.AddRange(nums);
+            return list.ToArray();
+        }
+
+        private static int[] GetConcatenationLinq(int[] nums)
+        {
+            return nums.Concat(nums).ToArray();
+        }
+    }
+}
