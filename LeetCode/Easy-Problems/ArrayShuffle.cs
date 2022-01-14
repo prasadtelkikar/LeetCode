84736af414f023967dc577fded1834466d625401
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-14 23:18:29 +0530

    Solved 2 easy questions

---
+++ LeetCode/Easy-Problems/ArrayShuffle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Easy_Problems
+{
+    public class ArrayShuffle
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            int[] result = Shuffle(nums, n);
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        private static int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[n*2];
+            for (int i = 0, j = 0; i < n; i++, j+=2)
+            {
+                result[j] = nums[i];
+                result[j + 1] = nums[n+i];
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/KidsWithCandies.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class KidsWithCandies
+    {
+        public static void Main(string[] args)
+        {
+            var candies = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var extraCandies = Convert.ToInt32(Console.ReadLine());
+
+            bool[] result = FindKidsWithHighestCandies(candies, extraCandies);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static bool[] FindKidsWithHighestCandies(int[] candies, int extraCandies)
+        {
+            bool[] highestCandies = new bool[candies.Length];
+            int maxCandies = candies.Max();
+            for(int i = 0; i < candies.Length; i++)
+            {
+                highestCandies[i] = (candies[i]+extraCandies) >= maxCandies;
+            }
+            return highestCandies;
+        }
+        private static bool[] FindKidsWithHightestCandies(int[] candies, int extraCandies)
+        {
+            var maxCandieCount = candies.Max();
+            return candies.Select(i => (i + extraCandies) > maxCandieCount).ToArray();
+        }
+    }
+}
