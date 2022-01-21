6e02942218e0c674ee15e43dc1f87f2ef0fdfd9f
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-21 23:29:53 +0530

    Tried N and double problem

---
+++ LeetCode/Easy-Problems/LongestCommonPrefix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class LongestCommonPrefix
+    {
+        public static void Main(string[] args)
+        {
+            var str = Console.ReadLine().Split(", ");
+            string result = LongestCommonPrefixFun(str);
+        }
+
+        private static string LongestCommonPrefixFun(string[] str)
+        {
+            var smallestString = str.OrderBy(x => x.Length).First();
+            var result = string.Empty;
+            for (int i = 0; i < smallestString.Length; i++)
+            {
+                var subSmallestString = smallestString.Substring(0, i + 1);
+                //if()
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/NAndDoubleExists.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class NAndDoubleExists
+    {
+        public static void Main(string[] args)
+        {
+            var inputArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var exists = CheckIfNAndDoubleExists(inputArr);
+        }
+
+        private static bool CheckIfNAndDoubleExists(int[] inputArr)
+        {
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                var doubleI = inputArr[i] * 2;
+                for (int j = 0; j < inputArr.Length; j++)
+                {
+                    if (doubleI == inputArr[j])
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        //private static bool CheckIfNAndDoubleExists(int[] inputArr)
+        //{
+            
+        //}
+    }
+}
