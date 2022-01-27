0774b28588ea443012bb4b5ac0eb6d53afcbec35
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-27 22:32:40 +0530

    Solved string problem

---
+++ LeetCode/Easy-Problems/DefangingIPAddress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Easy_Problems
+{
+    public class DefangingIPAddress
+    {
+        public static void Main(string[] args)
+        {
+            var inputStr = Console.ReadLine();
+            var outputStr = DefangIPaddr(inputStr);
+            Console.WriteLine(outputStr);
+        }
+
+        private static string DefangIPaddr(string inputStr)
+        {
+            var splitted = inputStr.Split('.');
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            foreach (var str in splitted)
+            {
+                if(count != splitted.Length)
+                    sb.Append($"{str}[.]");
+                else
+                    sb.Append(str);
+                count = count + 1;
+            }
+            return sb.ToString();
+        }
+
+        private static string DefangIPaddrLinq(string address)
+        {
+            return string.Join("[.]", address.Split('.'));
+        }
+    }
+}
