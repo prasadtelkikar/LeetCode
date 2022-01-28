46fd2194e19579b01e9c30371253bf05dc0e0360
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-28 23:30:14 +0530

    Solved array problem

---
+++ LeetCode/Easy-Problems/CountMatchingRule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class CountMatchingRule
+    {
+        public static void Main(string[] args)
+        {
+            IList<IList<string>> items = new List<IList<string>>();
+            for (int i = 0; i < 3; i++)
+            {
+                var item = Console.ReadLine().Split().ToList();
+                items.Add(item);
+            }
+            string ruleKey = Console.ReadLine();
+            string ruleValue = Console.ReadLine();
+
+            int result = CountMatches(items, ruleKey, ruleValue);
+        }
+
+        private static int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            string[] rules = new string[] { "type", "color", "name" };
+            var ruleIndex = 0;
+            for (int i = 0; i < rules.Length; i++)
+            {
+                if(rules[i] == ruleKey)
+                {
+                    ruleIndex = i;
+                    break;
+                }
+            }
+            var result = 0;
+            foreach (var item in items)
+            {
+                if(item[ruleIndex] == ruleValue)
+                    result++;
+            }
+            return result;
+        }
+        private static int CountMatchesLinq(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            var rule = new List<string> { "type", "color", "name" };
+            var index = rule.FindIndex(x => x == ruleKey);
+
+            var result = items.Count(x => x[index] == ruleValue);
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/RemoveElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class RemoveElement
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var val = Convert.ToInt32(Console.ReadLine());
+
+            var length = RemoveElementFun(nums, val);
+            Console.WriteLine(length);
+            Console.WriteLine(String.Join(' ', nums));
+        }
+
+        //Actual solution, which failed.
+        private static int RemoveElementFun(int[] nums, int val)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == val)
+                {
+                    nums[i] = int.MaxValue;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        //Cheated: Solution tab
+        public int RemoveElementFun1(int[] nums, int val)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
