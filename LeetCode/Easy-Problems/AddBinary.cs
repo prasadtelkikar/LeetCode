7364911956159dd8cd7511952dc814c09a03e22f
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-01 23:07:59 +0530

    Half cooked food, of no use

---
+++ LeetCode/Easy-Problems/AddBinary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class AddBinary
+    {
+        public static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            string result = AddBinaryFunc(a, b);
+            Console.WriteLine(result);
+        }
+
+        //Not supporting higher value. Need to write our own technique
+        private static string AddBinaryFunc(string a, string b)
+        {
+            long intA = Convert.ToInt64(a, 2);
+            long intB = Convert.ToInt64(b, 2);
+            long result = intA + intB;
+            return Convert.ToString(result, 2);
+        }
+    }
+}
