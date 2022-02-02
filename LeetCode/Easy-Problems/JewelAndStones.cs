44502f496656031e31b5077bd4fd29614acef1bb
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-02 23:14:09 +0530

    Solved one more easy problem

---
+++ LeetCode/Easy-Problems/JewelAndStones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class JewelAndStones
+    {
+        public static void Main(string[] args)
+        {
+            string jewels = Console.ReadLine();
+            string stones = Console.ReadLine();
+            int result = NumJewelsInStones(jewels, stones);
+        }
+
+        private static int NumJewelsInStones(string jewels, string stones)
+        {
+            int result = 0;
+            foreach(var ch in jewels.ToCharArray())
+                result += stones.Count(x => x.Equals(ch));
+            return result;
+        }
+    }
+}
