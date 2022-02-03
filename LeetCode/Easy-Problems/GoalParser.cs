5809df46d70bf05f23110b6fb1ada2e85adce077
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-03 22:31:19 +0530

    Solved two easy string problems

---
+++ LeetCode/Easy-Problems/GoalParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Easy_Problems
+{
+    public class GoalParser
+    {
+        public static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            string parsedCommand = Interpret(command);
+            Console.WriteLine(parsedCommand);
+        }
+
+        private static string Interpret(string command)
+        {
+            List<char> chars = new List<char>();
+            for (int i = 0; i < command.Length;)
+            {
+                if(command[i] == 'G')
+                {
+                    chars.Add(command[i]);
+                    i++; //Move forward by 1
+                }
+                else if(command[i] == '(' && command[i+1] == ')')
+                {
+                    chars.Add('o');
+                    i += 2; //Move forward by 2
+                }
+                else
+                {
+                    chars.AddRange(new [] { 'a', 'l' });
+                    i+=4;  //Move forward by 4
+                }
+            }
+            return new string(chars.ToArray());
+        }
+
+        //Cheated: Cleaver solution
+        private static string InterpretStringInbuildFunc(string command)
+        {
+            var result = command.Replace("()", "o").Replace("(al)", "al");
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/ReverseString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Easy_Problems
+{
+    public class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+            var inputCharArray = Console.ReadLine().ToCharArray();
+            ReverseStringFuncLinq(inputCharArray);
+        }
+
+        private static void ReverseStringFunc(char[] inputCharArray)
+        {
+            var length = inputCharArray.Length;
+            for (int i = 0; i < length/2; i++)
+            {
+                char temp = inputCharArray[i]; //space complexity O(1)
+                inputCharArray[i] = inputCharArray[length-i-1];
+                inputCharArray[length - i - 1] = temp;
+            }
+            Console.WriteLine(new String(inputCharArray));
+        }
+
+        //Not work, because it is not updating actual char []
+        private static void ReverseStringFuncLinq(char[] inputCharArray)
+        {
+            inputCharArray = inputCharArray.Reverse().ToArray();
+            Console.WriteLine(new String(inputCharArray));
+        }
+
+        private static void ReverseStringFuncArray(char[] inputCharArray)
+        {
+            Array.Reverse(inputCharArray);
+            Console.WriteLine(new String(inputCharArray));
+        }
+    }
+}
