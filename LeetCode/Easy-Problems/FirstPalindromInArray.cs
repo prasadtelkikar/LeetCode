87a5e30c6c4b13577d259816efe94c7eaf5cd866
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-04 23:55:23 +0530

    completed with bruteforce approach

--- LeetCode/Easy-Problems/FirstPalindromInArray.cs
+++ LeetCode/Easy-Problems/FirstPalindromInArray.cs
@@ -10,7 +10,19 @@ namespace Easy_Problems
     {
         public static void Main(string[] args)
         {
+            string[] words = Console.ReadLine().Split(' ');
+            string result = FindFirstPalindromInAnArray(words);
+        }
 
+        private static string FindFirstPalindromInAnArray(string[] words)
+        {
+            var reverseWords = words.Select(x => new string(x.Reverse().ToArray())).ToArray();
+            for(int i = 0; i < reverseWords.Length; i++)
+            {
+                if(words[i] == reverseWords[i])
+                    return reverseWords[i];
+            }
+            return null;
         }
     }
 }
