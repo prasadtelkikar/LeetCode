27b95b9331d44542c4c613f2b8e666e0f4d83c7a
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-23 23:09:40 +0530

    Solved two problems

---
+++ LeetCode/Easy-Problems/CountConsistentStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class CountConsistentStrings
+    {
+        public static void Main(string[] args)
+        {
+            string allowed = Console.ReadLine();
+            string[] words = Console.ReadLine().Split(' ');
+            int result = CountConsistentStringsInArray(allowed, words);
+            Console.WriteLine(result);
+        }
+
+        private static int CountConsistentStringsInArray(string allowed, string[] words)
+        {
+            var result = 0;
+            foreach (var word in words)
+            {
+                var distinctLettersInWord = word.Distinct();
+                if(distinctLettersInWord.All(x => allowed.Contains(x)))
+                {
+                        result++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/Pangram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class Pangram
+    {
+        public static void Main(string args)
+        {
+            string word = Console.ReadLine();
+            bool isPangram = IsPangram(word);
+        }
+
+        private static bool IsPangram(string word)
+        {
+            int[] wordCount = new int[26];
+            foreach (char letter in word)
+            {
+                wordCount[letter - 'a']++;
+            }
+            return wordCount.All(x => x == 1);
+        }
+    }
+}
