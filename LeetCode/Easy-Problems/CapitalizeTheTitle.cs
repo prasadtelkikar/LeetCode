110377a181a7188514d2d79f708567dafc0e357e
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-28 22:40:53 +0530

    Tried two easy string problems

---
+++ LeetCode/Easy-Problems/CapitalizeTheTitle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class CapitalizeTheTitle
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = CapitalizeTitle(input);
+            Console.WriteLine(output);
+        }
+
+        //buggy code, solve it easy fix
+        private static string CapitalizeTitle(string input)
+        {
+            return string.Join(' ',input.Split().Select(x => char.ToUpper(x[0]) + x.Substring(1).ToLower()));
+        }
+
+        //Solve same problem with different approach.
+    }
+}
---
+++ LeetCode/Easy-Problems/ToLowerCase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class ToLowerCase
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var output = ToLowerCaseFunc(input);
+        }
+
+        private static string ToLowerCaseFunc(string s)
+        {
+            return s.ToLower();
+        }
+
+        private static string ToLowerCaseForLoop(string s)
+        {
+            char[] word = new char[s.Length];
+            char[] upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            int index = 0;
+            foreach(var ch in s)
+            {
+                if(upperLetters.Contains(ch))
+                    word[index++] = char.ToLower(ch);
+                else
+                    word[index++] = ch;
+            }
+            return new string(word);
+        }
+    }
+}
