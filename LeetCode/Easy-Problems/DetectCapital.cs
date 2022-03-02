fa22a9f083e930d4bd49687aee2360b5f3d3af14
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-03-02 23:33:25 +0530

    Tried Detect Capital problem

---
+++ LeetCode/Easy-Problems/DetectCapital.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Easy_Problems
+{
+    public class DetectCapital
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isCapital = DetectCapitalUse(input);
+            Console.WriteLine(isCapital);
+        }
+
+        //Need clear understanding before I submit next attempt: https://leetcode.com/problems/detect-capital/
+        private static bool DetectCapitalUse(string word)
+        {
+            var subStr = word.Substring(1);
+            var regex = new System.Text.RegularExpressions.Regex(@"^[A-Z]+$");
+            return regex.IsMatch(word) || (char.IsUpper(word[0]) && subStr == subStr.ToLower());
+        }
+    }
+}
