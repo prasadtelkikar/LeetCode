5d8dc482c8aad9cd76c40a6644d64429af078607
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-03-04 23:54:55 +0530

    Solved easy problem related to string reverse

---
+++ LeetCode/Easy-Problems/ReverseVowelOfString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class ReverseVowelOfString
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = ReverseVowel(input);
+            Console.WriteLine(output);
+        }
+
+        //Can we make it in one for loop
+        private static string ReverseVowel(string input)
+        {
+            char[] vowels = "AEIOUaeiou".ToCharArray();
+            List<char> reverseActualVowels = new List<char>();
+            foreach(char ch in input)
+            {
+                if(vowels.Contains(ch))
+                    reverseActualVowels.Add(ch);
+            }
+            reverseActualVowels.Reverse();
+            List<char> result = new List<char>();
+            int i = 0;
+            foreach (char ch in input)
+            {
+                if (vowels.Contains(ch))
+                    result.Add(reverseActualVowels[i++]);
+                else
+                    result.Add(ch);
+            }
+            return new string(result.ToArray());
+        }
+    }
+}
