b8ece8e36e1cd5c92cb14f1f976af70c2870d1c2
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-03-12 22:58:13 +0530

    Solved easy problem Count prefix in array of word

---
+++ LeetCode/Easy-Problems/CountPrefix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class CountPrefix
+    {
+        public static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            string prefix = Console.ReadLine();
+            int result = PrefixCount(inputs, prefix);
+            Console.WriteLine(result);
+        }
+
+        private static int PrefixCount(string[] inputs, string prefix)
+        {
+            return inputs.Count(x => x.StartsWith(prefix));
+        }
+    }
+}
