0f169f67babad2e6aa279d28a22bf1257fcfc424
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-03-15 23:35:31 +0530

    Solved one more easy problem

---
+++ LeetCode/Easy-Problems/CountMatches.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class CountMatches
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = NumberOfMatches(n);
+            Console.WriteLine(result);
+        }
+
+        private static int NumberOfMatches(int n)
+        {
+            int ans = 0;
+            while(n > 1)
+            {
+                int temp = n / 2;
+                ans += temp;
+                n -= temp;
+            }
+            return ans;
+        }
+    }
+}
