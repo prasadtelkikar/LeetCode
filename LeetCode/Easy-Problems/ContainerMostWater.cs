8603b2df8949ff43b59c3e73e75548737a744432
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-05-12 00:01:08 +0530

    Brute-force solution

---
+++ LeetCode/Easy-Problems/ContainerMostWater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class ContainerMostWater
+    {
+        public static void Main(string[] args)
+        {
+            ContainerMostWater water = new ContainerMostWater();
+            var height = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int result = water.MaxArea(height);
+            Console.WriteLine(result);
+        }
+
+        private int MaxArea(int[] height)
+        {
+            var length = height.Length;
+            int maxArea = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for(int j = i+1; j < length; j++)
+                {
+                    var min = Math.Min(height[i], height[j]);
+                    var diff = j - i;
+                    var mul = min * diff;
+                    maxArea = Math.Max(maxArea, mul);
+                }
+            }
+            return maxArea;
+        }
+    }
+}
