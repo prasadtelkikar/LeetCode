25dc72c6624be9944747972b25eb6b71efec464b
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-05-24 23:03:33 +0530

    Incorrect solution for the Target Array in Given order problem

---
+++ LeetCode/Easy-Problems/TargetArrayInGivenOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class TargetArrayInGivenOrder
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var index = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            TargetArrayInGivenOrder targetArrayObj = new TargetArrayInGivenOrder();
+            var result = targetArrayObj.CreateTargetArray(nums, index);
+        }
+
+        private int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            Dictionary<int, List<int>> temp = new Dictionary<int, List<int>>();
+            List<int> list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var indexI = index[i];
+                var num = nums[i];
+                if(temp.ContainsKey(indexI))
+                    temp[indexI].Add(num);
+                else
+                    temp.Add(indexI, new List<int>() {num});
+
+            }
+            foreach(var kvp in temp)
+            {
+                kvp.Value.Reverse();
+                list.AddRange(kvp.Value);
+            }
+            return list.ToArray();
+        }
+    }
+}
