ed17de48c64174d5a905c4c8d031abae3ca3100f
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-05-25 23:27:06 +0530

    Solved two easy problems

---
+++ LeetCode/Easy-Problems/MinSeatMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class MinSeatMovement
+    {
+        public static void Main(string[] args)
+        {
+            var seats = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+            var students = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+            decimal diff = 0;
+            for (int i = 0; i < seats.Length; i++)
+            {
+                diff += Math.Abs(seats[i]- students[i]);
+            }
+            Console.WriteLine(diff);
+        }
+    }
+
+    
+}
---
+++ LeetCode/Easy-Problems/TruncateSentence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class TruncateSentence
+    {
+        public static void Main(string[] args)
+        {
+            var sentence = Console.ReadLine();
+            var takeCount = int.Parse(Console.ReadLine());
+            var result = string.Join(" ", sentence.Split(' ').Take(takeCount));
+            Console.WriteLine(result);
+        }
+    }
+}
