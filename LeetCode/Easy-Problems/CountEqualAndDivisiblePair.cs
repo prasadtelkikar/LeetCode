95d64e2a82cca3e929ce41fc49dc41f4d1419a8b
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-05-31 09:21:30 +0530

    Solved 2 easy array problems

---
+++ LeetCode/Easy-Problems/CountEqualAndDivisiblePair.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class CountEqualAndDivisiblePair
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').ToArray();
+            var k = int.Parse(Console.ReadLine());
+
+            int result = CountPairs(nums, k);
+        }
+
+        private static int CountPairs(string[] nums, int k)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[i] == nums[j] && (i * j % 2 == 0))
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/UniqueMorseCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class UniqueMorseCode
+    {
+        public static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(',');
+            int result = UniqueMorseRepresentations(words);
+            Console.WriteLine(result);
+        }
+
+        private static int UniqueMorseRepresentations(string[] words)
+        {
+            var morseCodes = new string[] {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
+                "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+            var temp = new HashSet<string>();
+
+            foreach (string word in words)
+            {
+                var morseCode = string.Join("", word.Select(x => morseCodes[x - 'a']));
+                temp.Add(morseCode);
+            }
+            return temp.Count();
+        }
+    }
+}
