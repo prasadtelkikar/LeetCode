a47ab5e7d2d8de5ec8fcb7b9d176bc987af6fc54
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-06-01 09:24:19 +0530

    Started June challenge, tried to solve one array problem

---
+++ LeetCode/Easy-Problems/SumOfOddLengthArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class SumOfOddLengthArray
+    {
+        public static void Main(string[] args)
+        {
+            var inputArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var outputArray = SumOddLengthSubarrays(inputArray);
+            Console.WriteLine(outputArray);
+        }
+
+        //Not Working
+        private static int SumOddLengthSubarrays(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i+1; j < inputArray.Length; j++)
+                {
+                    int diff = j - i;
+                    if(diff % 2 != 0)
+                    {
+                        sum += inputArray.Select((x, z) => z >= i && z < j ? x : 0).Sum();
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/June-Month-Challenge/June-2022/SumOneDArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June_2022
+{
+    public class SumOneDArray
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] result = RunningSum(nums);
+            Console.WriteLine(result);
+        }
+
+        private static int[] RunningSum(int[] nums)
+        {
+            var output = new int[nums.Length];
+            output[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                output[i] = output[i-1] + nums[i];
+
+            return output;
+        }
+    }
+}
