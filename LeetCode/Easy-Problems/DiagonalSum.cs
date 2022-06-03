3a47b5421e47096e2d074d48f298ce5f5a7e25fa
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-06-03 23:35:56 +0530

    Solved one more easy question

---
+++ LeetCode/Easy-Problems/DiagonalSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class DiagonalSum
+    {
+        public static void Main(string[] args)
+        {
+            int[][] arr = new int[3][];
+            arr[0] = new int[3] {1, 2, 3};
+            arr[1] = new int[3] {4, 5, 6};
+            arr[2] = new int[3] {7, 8, 9};
+            int result = DiagonalSumFun(arr);
+        }
+
+        private static int DiagonalSumFun(int[][] mat)
+        {
+            var length = mat.Length;
+            var majorDiagonalSum = 0;
+            var minorDiagonalSum = 0;   
+            for (int i = 0; i < length; i++)
+            {
+                majorDiagonalSum+=mat[i][i];
+                var column = length - i - 1;
+                if (column != i)
+                    minorDiagonalSum += mat[i][column];
+            }
+            return majorDiagonalSum + minorDiagonalSum;
+        }
+    }
+}
---
+++ LeetCode/June-Month-Challenge/June-2022/RangeSum2D.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June_2022
+{
+    public class RangeSum2D
+    {
+        //TODO: Check it tomorrow
+        public static void Main(string[] args)
+        {
+            int[][] matrix = new int[][]
+        }
+    }
+}
