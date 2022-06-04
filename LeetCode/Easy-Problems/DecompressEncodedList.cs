35415537c920b5c38f743fdfcd81ce4ffa674340
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-06-04 22:09:47 +0530

    Solved one more easy question

---
+++ LeetCode/Easy-Problems/DecompressEncodedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class DecompressEncodedList
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] result = DecompressRLElist(nums);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int[] DecompressRLElist(int[] nums)
+        {
+            List<int> result = new List<int>(); 
+            for(int i = 0; i < nums.Length; i += 2)
+            {
+                var freq = nums[i];
+                var val = nums[i + 1];
+                for (int j = 0; j < freq; j++)
+                    result.Add(val);
+            }
+            return result.ToArray();
+        }
+    }
+}
