5c56e8796a7e79f01b083943e1fc427db0835eda
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-06-20 22:40:47 +0530

    Solved two medium problem by own

---
+++ LeetCode/June-Month-Challenge/June-2022/SearchSuggestionSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June_2022
+{
+    public class SearchSuggestionSystem
+    {
+        public static void Main(string[] args)
+        {
+            string[] products = new string[5] { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
+            string searchWord = "mouse";
+            IList<IList<string>> result = SuggestedProducts(products, searchWord);
+            foreach (var item in result)
+            {
+                Console.WriteLine(string.Join(", ", item));
+            }
+            Console.ReadLine();
+        }
+
+        private static IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+        {
+            IList<IList<string>> result = new List<IList<string>>();
+            for (int i = 0; i < searchWord.Length; i++)
+            {
+                var subString = searchWord.Substring(0, i+1);
+                var product = products.Where(x => x.StartsWith(subString)).OrderBy(x => x).Take(3).ToList();
+                result.Add(product);
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/June-Month-Challenge/June-2022/WordShortEncoding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June_2022
+{
+    public class WordShortEncoding
+    {
+        public static void Main(string[] args)
+        {
+            string[] words = new string[] { "time", "me", "bell" };
+            int count = MinimumLengthEncoding(words);
+            Console.WriteLine(count);
+        }
+
+        //Satisfaction.... 😊
+        private static int MinimumLengthEncoding(string[] words)
+        {
+            var temp = words.ToHashSet().OrderByDescending(x => x.Length).ToList();
+            var length = temp.Count;
+            int sum = 0;
+            var list = new HashSet<string>();
+            for (int i = length-1; i >= 0; i--)
+            {
+                var t = temp.FirstOrDefault(x => x.EndsWith(temp[i]));
+                if (t != null)
+                    list.Add(t);
+                else
+                    list.Add(temp[i]);
+            }
+            foreach (var t in list)
+                sum += t.Length + 1;
+            return sum;
+        }
+    }
+}
