c1f7d954d957a6b2a06028730379b7aca97c6298
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-06-27 23:44:54 +0530

    Failed attempt for dream, solved DeciBinary

---
+++ LeetCode/Dream/MinimumWindowSubstring.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dream
+{
+    public class MinimumWindowSubstring
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string subStr = Console.ReadLine();
+            string output = MinWindow(input, subStr);
+            Console.WriteLine(output);
+        }
+
+        private static string MinWindow(string input, string subStr)
+        {
+            int left = 0;
+            int right = 0;
+            int minLength = int.MaxValue;
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            while(left <= right && right < input.Length)
+            {
+                string subString = string.Empty;
+                if (left+right+1 > input.Length)
+                    subString = input.Substring(left, input.Length - left - 1);
+                else
+                    subString = input.Substring(left, right+1);
+
+                if (subStr.All(x => subString.Contains(x)))
+                {
+                    minLength = Math.Min(minLength, subString.Length);
+                    if(keyValuePairs.ContainsKey(subString))
+                    {
+                        minLength = Math.Min(minLength, keyValuePairs[subString]);
+                        keyValuePairs[subString] = minLength;
+                    }
+                    else
+                        keyValuePairs.Add(subString, minLength);
+                    left++;
+                }
+                else
+                    right++;
+            }
+
+            string result = string.Empty;
+            int valueMin = int.MaxValue;
+            foreach (var kvp in keyValuePairs)
+            {
+                if(kvp.Value < valueMin)
+                    result = kvp.Key;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/June-Month-Challenge/June-2022/DeciBinary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June_2022
+{
+    public class DeciBinary
+    {
+        public static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            int result = MinPartitions(n);
+            Console.WriteLine(result);
+        }
+
+        private static int MinPartitions(string n)
+        {
+            int minOnce = int.MinValue;
+            for (int i = 0; i < n.Length; i++)
+            {
+                int value = n[i] - '0';
+                if (value == 9) return 9;
+                else
+                    minOnce = Math.Max(minOnce, value);
+            }
+            return minOnce;
+        }
+    }
+}
