68cd0c23cb2885cc5ae2e6705d7309acd6e5d052
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-07-04 08:50:16 +0530

    Solved two questions

---
+++ LeetCode/Dream/TrappingRainWater.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dream
+{
+    public class TrappingRainWater
+    {
+        public static void Main(string[] args)
+        {
+            int[] height = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int result = TrapTwoPointer(height);
+            Console.WriteLine(result);
+        }
+
+        //Using two lists, MaxR and MaxL
+        private static int Trap(int[] height)
+        {
+            //Step1: Calculate MaxLeft
+            int maxLeft = int.MinValue;
+            Dictionary<int, int> indexWithMaxLeft = new Dictionary<int, int>();
+            //For first left element, max left height will be 0 always
+            indexWithMaxLeft.Add(0, 0); 
+            for (int i = 1; i < height.Length; i++)
+            {
+                maxLeft = Math.Max(maxLeft, height[i-1]);
+                indexWithMaxLeft.Add(i,maxLeft);
+            }
+
+            //Step 2: Calculate maximum right height
+            int maxRight = int.MinValue;
+            Dictionary<int, int> indexWithMaxRight = new Dictionary<int, int>();
+            //For first right hand side element, height will be always zero
+            indexWithMaxRight.Add(height.Length - 1, 0);
+            for (int i = height.Length - 2; i >= 0; i--)
+            {
+                maxRight = Math.Max(maxRight, height[i+1]);
+                indexWithMaxRight.Add(i, maxRight);
+            }
+
+            //Find Minimum from Left max height and right max height
+            List<int> MinLeftRigthHeight = new List<int>();
+            for(int i = 0; i < height.Length; i++)
+            {
+                int min = Math.Min(indexWithMaxLeft[i], indexWithMaxRight[i]);
+                MinLeftRigthHeight.Add(min);
+            }
+
+            //Now, calculate trapped water
+            int result = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                int temp = MinLeftRigthHeight[i] - height[i];
+                result += temp > 0 ? temp : 0;
+            }
+            return result;
+        }
+
+        //Two pointers problem
+        private static int TrapTwoPointer(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxLeft = height[left];
+            int maxRight = height[right];
+            int result = 0;
+
+            while (left < right)
+            {
+                if(maxLeft < maxRight)
+                {
+                    left++;
+                    maxLeft = Math.Max(maxLeft, height[left]);
+                    result += maxLeft - height[left];
+                }
+                else
+                {
+                    right--;
+                    maxRight = Math.Max(maxRight, height[right]);
+                    result += maxRight - height[right]; 
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Easy-Problems/ProductArrayExceptSelf.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class ProductArrayExceptSelf
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] result = ProductExceptSelf(nums);
+            Console.WriteLine(result);
+        }
+
+        private static int[] ProductExceptSelf(int[] nums)
+        {
+            int[] left = new int[nums.Length];
+            int[] right = new int[nums.Length];
+
+            left[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+                left[i] = left[i-1] * nums[i-1];
+
+            //Set boundary to 1
+            right[right.Length - 1] = 1;
+            for (int i = right.Length - 2; i >= 0; i--)
+                right[i] = right[i + 1] * nums[i + 1];
+
+            int[] product = new int[nums.Length];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                product[i] = left[i] * right[i];
+            }
+            return product;
+        }
+    }
+}
