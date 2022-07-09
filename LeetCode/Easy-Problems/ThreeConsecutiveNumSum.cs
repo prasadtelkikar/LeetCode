2a200c6e53dd524f40c38f059228dde3c7d8ed47
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-07-09 15:30:41 +0530

    Solved second question related to Spiral Matrix

---
+++ LeetCode/Easy-Problems/ThreeConsecutiveNumSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Problems
+{
+    public class ThreeConsecutiveNumSum
+    {
+        public static void Main(string[] args)
+        {
+
+            long num = long.Parse(Console.ReadLine());
+            long[] result = SumOfThree(num);
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        private static long[] SumOfThree(long num)
+        {
+            long rem = num % 3;
+            if(rem != 0)
+                return new long[0];
+            else
+            {
+                long x = num / 3;
+                return new long[] { x - 1, x, x + 1 };
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode-Medium/SpiralMatrix-II.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode_Medium
+{
+    public class SpiralMatrix_II
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[][] result = GenerateMatrix(n);
+            for(int row = 0; row < n; row++)
+            {
+                for(int col = 0; col < n; col++)
+                    Console.Write(result[row][col] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        private static int[][] GenerateMatrix(int n)
+        {
+            int count = 1;
+            int[][] matrix = new int[n][];
+            for(int i = 0; i < n; i++)
+                matrix[i] = new int[n];
+
+            int up = 0, left = 0;
+            int down = n - 1, right = n - 1;
+
+            while(up <= down && left <= right)
+            {
+                for (int col = up; col <= down; col++)
+                    matrix[up][col] = count++;
+                for (int row = up+1; row <= right; row++)
+                    matrix[row][right] = count++;
+
+                if(up < down)
+                {
+                    for(int col = down-1; col >= up; col--)
+                        matrix[down][col] = count++;
+                }
+                if(left < right)
+                {
+                    for (int row = down-1; row > up; row--)
+                        matrix[row][left] = count++;
+                }
+
+                up++;
+                down--;
+                left++;
+                right--;
+            }
+            return matrix;
+        }
+    }
+}
